7d5456900c6d1a48a35f3367d7fa3b58d4eeecef
Vlado Senasi <vladimir.senasi@nalgoo.com>
2019-10-09 19:37:07 +0200

    added missing interface

--- src/Application/Middleware/ReverseProxyMiddleware.php
+++ src/Application/Middleware/ReverseProxyMiddleware.php
@@ -5,9 +5,10 @@ namespace Nalgoo\Common\Application\Middleware;
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
-class ReverseProxyMiddleware
+class ReverseProxyMiddleware implements MiddlewareInterface
 {
 	public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
 	{
