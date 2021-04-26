e29ea6428b7451e2fe2e1830f4b019b55a88989a
Vlado Senasi <vladimir.senasi@nalgoo.com>
2021-04-26 13:45:31 +0200

    deprecating nullable enum

--- src/Domain/Enums/NullableStringEnum.php
+++ src/Domain/Enums/NullableStringEnum.php
@@ -3,6 +3,9 @@ declare(strict_types=1);
 
 namespace Nalgoo\Common\Domain\Enums;
 
+/**
+ * @deprecated This does not make sense, please don't use it, sorry :)
+ */
 abstract class NullableStringEnum extends Enum
 {
 	public function __construct(?string $value)
