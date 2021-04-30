69903cc954f6deb08c4449e0f4913bdfe1991563
Ondrej Satala <ondrej.satala@nalgoo.com>
2021-04-30 16:23:38 +0200

    added api key middleware

---
+++ src/Application/Middleware/Exceptions/ApiKeyMiddlewareException.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace Nalgoo\Common\Application\Middleware\Exceptions;
+
+use Nalgoo\Common\Application\Exceptions\AuthorizationException;
+
+class ApiKeyMiddlewareException extends AuthorizationException
+{
+
+}
