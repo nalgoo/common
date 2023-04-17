aa2ec78c007186e1b61687c8b407aeb1ba6cd1de
Ondrej Šatala <ondrej.satala@nalgoo.com>
2023-04-17 15:50:15 +0200

    Migrated doctrine annotations to attributes

--- src/Domain/SequenceIdentifierTrait.php
+++ src/Domain/SequenceIdentifierTrait.php
@@ -14,11 +14,11 @@ use Symfony\Component\Serializer\Annotation\Groups;
 trait SequenceIdentifierTrait
 {
 	/**
-	 * @ORM\Column(name="id", type="integer", length=10, nullable=false, options={"unsigned"=true})
-	 * @ORM\Id
-	 * @ORM\GeneratedValue(strategy="AUTO")
 	 * @OA\Property()
 	 */
 	#[Groups([SerializerInterface::LIST_GROUP])]
+	#[ORM\Column(name: 'id', type: 'integer', length: 10, nullable: false, options: ['unsigned' => true])]
+	#[ORM\Id]
+	#[ORM\GeneratedValue(strategy: 'AUTO')]
 	protected int $id;
 }
--- src/Domain/StringIdentifierTrait.php
+++ src/Domain/StringIdentifierTrait.php
@@ -14,12 +14,12 @@ use Symfony\Component\Serializer\Annotation\Groups;
 trait StringIdentifierTrait
 {
 	/**
-	 * @ORM\Column(name="id", type="string", length=24, nullable=false, options={"fixed"=true, "collation"="ascii_bin"})
-	 * @ORM\Id
-	 * @ORM\GeneratedValue(strategy="NONE")
 	 * @OA\Property()
 	 */
 	#[Groups([SerializerInterface::LIST_GROUP])]
+	#[ORM\Column(name: 'id', type: 'string', length: 24, nullable: false, options: ['fixed' => true, 'collation' => 'ascii_bin'])]
+	#[ORM\Id]
+	#[ORM\GeneratedValue(strategy: 'NONE')]
 	protected string $id;
 
 	public function getId(): StringIdentifier
