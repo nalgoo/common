f89446c56a27b2837a660e4070508b63e91d7934
Ondrej Šatala <ondrej.satala@nalgoo.com>
2023-05-02 10:30:33 +0200

    Migrated openapi annotations to attributes

--- src/Domain/SequenceIdentifierTrait.php
+++ src/Domain/SequenceIdentifierTrait.php
@@ -5,20 +5,16 @@ namespace Nalgoo\Common\Domain;
 
 use Doctrine\ORM\Mapping as ORM;
 use Nalgoo\Common\Application\Interfaces\SerializerInterface;
-use OpenApi\Annotations as OA;
+use OpenApi\Attributes as OA;
 use Symfony\Component\Serializer\Annotation\Groups;
 
-/**
- * @OA\Schema()
- */
+#[OA\Schema]
 trait SequenceIdentifierTrait
 {
-	/**
-	 * @OA\Property()
-	 */
 	#[Groups([SerializerInterface::LIST_GROUP])]
 	#[ORM\Column(name: 'id', type: 'integer', length: 10, nullable: false, options: ['unsigned' => true])]
 	#[ORM\Id]
 	#[ORM\GeneratedValue(strategy: 'AUTO')]
+	#[OA\Property]
 	protected int $id;
 }
