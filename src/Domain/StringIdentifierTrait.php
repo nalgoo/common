5f1649b3abcafd99cc33655a4e4943ca31434eac
satalaondrej <40560686+satalaondrej@users.noreply.github.com>
2023-05-02 17:54:50 +0200

    Migrated openapi annotations to attributes (#32)
    Co-authored-by: Ondrej Šatala <ondrej.satala@nalgoo.com>

--- src/Domain/StringIdentifierTrait.php
+++ src/Domain/StringIdentifierTrait.php
@@ -5,21 +5,17 @@ namespace Nalgoo\Common\Domain;
 
 use Doctrine\ORM\Mapping as ORM;
 use Nalgoo\Common\Application\Interfaces\SerializerInterface;
-use OpenApi\Annotations as OA;
+use OpenApi\Attributes as OA;
 use Symfony\Component\Serializer\Annotation\Groups;
 
-/**
- * @OA\Schema()
- */
+#[OA\Schema]
 trait StringIdentifierTrait
 {
-	/**
-	 * @OA\Property()
-	 */
 	#[Groups([SerializerInterface::LIST_GROUP])]
 	#[ORM\Column(name: 'id', type: 'string', length: 24, nullable: false, options: ['fixed' => true, 'collation' => 'ascii_bin'])]
 	#[ORM\Id]
 	#[ORM\GeneratedValue(strategy: 'NONE')]
+	#[OA\Property]
 	protected string $id;
 
 	public function getId(): StringIdentifier
