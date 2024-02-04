59116609c4438a1e577f9429fafe315df3b80291
Vlado Senasi <vladimir.senasi@nalgoo.com>
2024-02-04 19:59:49 +0100

    Added IdentifierNormalizer

---
+++ src/Application/Normalizers/IdentifierNormalizer.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace Nalgoo\Common\Application\Normalizers;
+
+use Nalgoo\Common\Domain\IntegerIdentifier;
+use Nalgoo\Common\Domain\StringIdentifier;
+use Symfony\Component\Serializer\Exception\InvalidArgumentException;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+
+class IdentifierNormalizer implements DenormalizerInterface
+{
+	public function denormalize($data, string $type, string $format = null, array $context = [])
+	{
+		if (!$this->supportsDenormalization($data, $type)) {
+			throw new InvalidArgumentException();
+		}
+
+		return new $type($data);
+	}
+
+	public function supportsDenormalization($data, string $type, string $format = null): bool
+	{
+		return (is_subclass_of($type, StringIdentifier::class) && is_string($data))
+			|| (is_subclass_of($type, IntegerIdentifier::class) && is_int($data));
+	}
+}
