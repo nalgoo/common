4d2e6c75007e987c23fee3564079fdd8e67cefba
Vlado Senasi <vladimir.senasi@nalgoo.com>
2024-05-06 14:25:02 +0200

    set previous exception for Persistence exceptions

--- src/Infrastructure/Persistence/PersistenceException.php
+++ src/Infrastructure/Persistence/PersistenceException.php
@@ -11,9 +11,9 @@ class PersistenceException extends \Exception
 	public static function from(\Throwable $e): self
 	{
 		return match (get_class($e)) {
-			ConnectionException::class => new Exceptions\ConnectionException($e->getMessage()),
-			UniqueConstraintViolationException::class => new Exceptions\UniqueConstraintViolationException($e->getMessage()),
-			default => new self($e->getMessage()),
+			ConnectionException::class => new Exceptions\ConnectionException($e->getMessage(), $e->getCode(), $e),
+			UniqueConstraintViolationException::class => new Exceptions\UniqueConstraintViolationException($e->getMessage(), $e->getCode(), $e),
+			default => new self($e->getMessage(), $e->getCode(), $e),
 		};
 	}
 }
